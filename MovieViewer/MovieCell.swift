4af69d7c0410d19a4d5044a5794dfbed341ad261
Kate <katesuttner@gmail.com>
2016-01-14 21:03:19 -0800

    Initial Week 1 Basic App

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Kate Suttner on 1/9/16.
+//  Copyright © 2016 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+}
